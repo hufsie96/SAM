7dc847de7cca167f3a8053a74cc25ba8c1875fa8
hufsie96 <hufsie96@naver.com>
2021-07-02 11:39:42 +0900

    modify pom for maven test step

--- management/src/test/java/com/kep/cnp/sam/management/ManagementApplicationTests.java
+++ management/src/test/java/com/kep/cnp/sam/management/ManagementApplicationTests.java
@@ -2,8 +2,10 @@ package com.kep.cnp.sam.management;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("local")
 class ManagementApplicationTests {
 
     @Test
