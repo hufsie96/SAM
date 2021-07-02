becf679215d29ec18a47bcc5203ceab33746eddb
hufsie96 <hufsie96@naver.com>
2021-07-02 11:40:28 +0900

    modify for maven test task

--- manageraccount/src/test/java/com/kep/cnp/sam/manageraccount/ManageraccountApplicationTests.java
+++ manageraccount/src/test/java/com/kep/cnp/sam/manageraccount/ManageraccountApplicationTests.java
@@ -2,8 +2,10 @@ package com.kep.cnp.sam.manageraccount;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("local")
 class ManageraccountApplicationTests {
 
     @Test
