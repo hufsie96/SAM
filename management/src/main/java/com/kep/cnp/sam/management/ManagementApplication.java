ea123ba335bafc11d40551b95f16957ab66138a4
won choi <34291208+hufsie96@users.noreply.github.com>
2021-07-27 15:54:26 +0900

    Update ManagementApplication.java

--- management/src/main/java/com/kep/cnp/sam/management/ManagementApplication.java
+++ management/src/main/java/com/kep/cnp/sam/management/ManagementApplication.java
@@ -2,7 +2,7 @@ package com.kep.cnp.sam.management;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//main
 @SpringBootApplication
 public class ManagementApplication {
 
